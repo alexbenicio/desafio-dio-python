44536a11427137b6f783b3143748a52633f7e1af
Alex Benício <alexibc2019@gmail.com>
2024-04-03 17:52:37 -0300

    Commit inicial

---
+++ app.py
@@ -0,0 +1,21 @@
+'''
+DESAFIO DIO
+
+Neste projeto, você terá a oportunidade de criar um Sistema Bancário em Python. 
+O objetivo é implementar três operações essenciais: depósito, saque e extrato. 
+O sistema será desenvolvido para um banco que busca monetizar suas operações. 
+Durante o desafio, você terá a chance de aplicar seus conhecimentos em programação Python 
+e criar um sistema funcional que simule as operações bancárias. 
+Prepare-se para aprimorar suas habilidades e demonstrar sua capacidade de desenvolver soluções práticas e eficientes.
+
+REGRAS
+* LIMITE DE 3 SAQUES DIARIOS
+* LIMITE DE R$ 500,00 POR SAQUE
+* CONFIGURAR EXIBIÇÃO DOS NUMERÁRIOS DA SEGUINTE FORMA: R$ XXXX.XX
+* NÃO É PERMITIDO DEPOSITO DE VALORES NEGATIVOS
+* NÃO É PERMITIDO SAQUE COM VALOR SUPERIOR AO SALDO
+
+'''
+
+
+
