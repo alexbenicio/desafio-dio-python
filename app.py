0e881668ad9bddc39c0d5f2aefa5d0a14334b71e
Alex Benício <alexibc2019@gmail.com>
2024-04-04 12:13:55 -0300

    Implementado funcionalidades de SAQUE, EXTRATO e DEPOSITO

--- app.py
+++ app.py
@@ -17,28 +17,78 @@ REGRAS
 
 '''
 
-# Controles
-VL_LIMITE_SAQUE = 500
-QT_LIMITE_SAQUE = 3
-SALDO_INICIAL = 0
+class ContaUnica:
+    def __init__(self, vl_limite_saque, qt_limite_saque, saldo, numero_saques, extrato_detalhe):
+        self.vl_limite_saque = vl_limite_saque
+        self.qt_limite_saque = qt_limite_saque
+        self.saldo = saldo
+        self.numero_saques = numero_saques
+        self.extrato_detalhe = extrato_detalhe
+    # Métodos 
+    def saque(self):
+        vl_numerario = float(input(("Informe o valor do saque: ")))
+        if self.numero_saques < self.qt_limite_saque:
+            if vl_numerario <= self.vl_limite_saque:
+                if vl_numerario <= self.saldo:
+                    self.saldo -= vl_numerario
+                    self.extrato_detalhe += f"\n    Saque: R$ {vl_numerario:.2f}\n"
+                    print("Saque realizado com sucesso!")
 
-# Variaveis
-Saldo = 0
-VlNumerario = 0
-ContaCorrente = 0
-Agencia = 1
-numeroSaques = 0
+                    # Incremento de quantidade de saques realizados
+                    self.numero_saques += 1
+                else:
+                    print("""
+    Falha na operação. 
+    Motivo:  Saldo insuficiente. Por 
+    favor, tente novamente com outro
+    valor. 
+                    """)
+        
+            else:
+                print("""
+    Falha na operação. 
+    Motivo:  Valor máximo para saque 
+    é de R$ 500,00 por operação. Por 
+    favor, tente novamente com outro
+    valor. 
+                """)
 
-# Métodos 
+        else:
+            print("""\n
+    Falha na operação. 
+    Motivo: Você já esgotou o total 
+    de  3 saques autorizados para o 
+    dia de hoje. 
+    Tente novamente amanhã.
+            """)
 
-def saque():
-    print("Iniciando operação de saque.")
+    def deposito(self):
+        vl_numerario = float(input(("Informe o valor do depósito: ")))
 
-def deposito():
-    print("Iniciando operação de depósito")
+        if vl_numerario > 0:
+            self.saldo += vl_numerario
+            self.extrato_detalhe += f"\n    Depósito: R$ {vl_numerario:.2f}\n"
+            print("Depósito realizado com sucesso!")
+        else:
+                        print("""\n
+    Falha na operação. 
+    Motivo: Valor de depósito inválido.
+                        """)
 
-def extrato():
-    print("Aguarde a impressão do Extrato")
+    def extrato(self):
+        print(f"""
+    *****************************
+    *****************************
+        Banco Digital AB
+    -----------------------------
+            EXTRATO
+    {self.extrato_detalhe}
+
+    SALDO ATUAL: {self.saldo}
+    *****************************
+    *****************************
+
+        """)
 
 def menu():
     print("""
@@ -58,17 +108,33 @@ def menu():
     """)
 
 def main():
+    #Configurando conta única
+    # Controles
+    VL_LIMITE_SAQUE = 500
+    QT_LIMITE_SAQUE = 3
+    SALDO = 0
+
+    # Variaveis
+    numero_saques = 0
+    extrato_detalhe = """
+    -----------------------------
+    """
+    conta = ContaUnica(VL_LIMITE_SAQUE,
+                       QT_LIMITE_SAQUE,
+                       SALDO,
+                       numero_saques,
+                       extrato_detalhe)
     while True:
         menu()
         opcao = input("Digite a opção desejada: ")
         opcao = opcao.upper()
 
         if opcao == 'S':
-            saque()
+            conta.saque()
         elif opcao == 'D':
-            deposito()
+            conta.deposito()
         elif opcao == 'E':
-            extrato()
+            conta.extrato()
         elif opcao == 'Q':
             print("Sessão encerrada.")
             break
